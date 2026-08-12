08f41b6d63d39f61abd62705a696c0b29a278120
jacobeatsspam <jacobeatsspam@users.noreply.github.com>
2026-08-12 17:40:01 -0500

    kubectl

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -34,6 +34,35 @@ fi
 
 mv "$tmpfile" "$CONFIG_PATH"
 
+# Configure kubectl for in-cluster service account access
+if [ -n "${KUBERNETES_SERVICE_HOST:-}" ] && [ -n "${KUBERNETES_SERVICE_PORT:-}" ] \
+  && [ -f /var/run/secrets/kubernetes.io/serviceaccount/token ] \
+  && [ -f /var/run/secrets/kubernetes.io/serviceaccount/ca.crt ] \
+  && [ -f /var/run/secrets/kubernetes.io/serviceaccount/namespace ]; then
+
+  SA_DIR=/var/run/secrets/kubernetes.io/serviceaccount
+  KCFG=/tmp/incluster-kubeconfig
+  API_SERVER="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}"
+  TOKEN="$(cat "$SA_DIR/token")"
+  NAMESPACE="$(cat "$SA_DIR/namespace")"
+
+  kubectl config --kubeconfig="$KCFG" set-cluster incluster \
+    --server="$API_SERVER" \
+    --certificate-authority="$SA_DIR/ca.crt" \
+    --embed-certs=true >/dev/null
+
+  kubectl config --kubeconfig="$KCFG" set-credentials service-account \
+    --token="$TOKEN" >/dev/null
+
+  kubectl config --kubeconfig="$KCFG" set-context incluster \
+    --cluster=incluster \
+    --user=service-account \
+    --namespace="$NAMESPACE" >/dev/null
+
+  kubectl config --kubeconfig="$KCFG" use-context incluster >/dev/null
+  export KUBECONFIG="$KCFG"
+fi
+
 # if thrown flags immediately,
 # assume they want to run the blockchain daemon
 if [ "$(printf '%s' "$1" | cut -c 1)" = '-' ]; then
